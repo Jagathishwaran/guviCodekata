bcff7a64e4a21739ebfbc2c0533cabab8397b003
Jagathishwaran <31395312+Jagathishwaran@users.noreply.github.com>
2018-07-12 14:23:29 +0530

    Update PositiveNegativeTest.py

--- PositiveNegativeTest.py
+++ PositiveNegativeTest.py
@@ -1,14 +1,14 @@
 inp = input()
 try:
-  inp = int(inp)
-expect:
-  pass
+    inp = int(inp)
+except:
+    pass
 if type(inp) is str:
-  print('Invalid Input')
+    print('Invalid Input')
 else:
-  if inp > 0:
-    print('Positive')
-  elif inp<0:
-    print('Negative')
-  else:
-    print('Zero')
+    if inp > 0:
+        print('Positive')
+    elif inp < 0:
+        print('Negative')
+    else:
+        print('Zero')
