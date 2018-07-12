13cb575f24664b3285ba9df9f21fda123879c3a2
Jagathishwaran <31395312+Jagathishwaran@users.noreply.github.com>
2018-07-12 14:48:57 +0530

    Delete vowelOrConsonant.py

--- vowelOrConsonant.py
+++
@@ -1,9 +0,0 @@
-inp = input()
-vowels = 'aeiouAEIOU'
-if len(inp) == 1 and inp.isalpha():
-    if inp in vowels:
-        print('vowel')
-    else:
-        print('Consonant')
-else:
-    print('Invalid')
